ab611e0e0c4bb48ab23e30759062bf9b1c660f14
Nuno Alexandre <bnunoalexandre@deloitte.co.uk>
2023-08-03 17:16:46 +0100

    fixes a few indentation and typo errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,7 +8,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     """Test models."""
 
-    def test_create_user_email_successful(self):
+    def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
         email = 'test@example.com'
         password = 'testpass123'
