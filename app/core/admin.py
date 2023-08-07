5a27cd7b4bb3d0c847698439f1461bffb4dca84b
Nuno Alexandre <bnunoalexandre@deloitte.co.uk>
2023-08-07 09:47:52 +0100

    adds logic for create user page

--- app/core/admin.py
+++ app/core/admin.py
@@ -25,9 +25,23 @@ class UserAdmin(BaseUserAdmin):
                 )
             }
         ),
-        (_('Important dates'), {'fields': ('last_login',)})
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_staff',
+                'is_active',
+                'is_superuser',
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
