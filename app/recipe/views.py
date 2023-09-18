7b8efc37e91fce29bd50fdd8d9c580dbfd28e51a
Nuno Alexandre <bnunoalexandre@deloitte.co.uk>
2023-09-18 20:43:20 +0100

    flake8 fixes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -90,7 +90,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
